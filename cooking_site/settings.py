fe9f7169213d11ec7b314cfb575a9b7b79d89f80
levstepansin-ship-it <levstepansin@gmail.com>
2026-06-03 15:53:21 +0300

    Fixed ALLOWED_HOSTS

--- cooking_site/settings.py
+++ cooking_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fvqc)^$n9zq65ak*n_)$5%!kolj5$%342e!2ea9w@g_s*yzz=l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
