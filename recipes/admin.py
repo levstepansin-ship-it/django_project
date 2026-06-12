47e8bf21e1f91ba48a8327eb183b0c3abf91c200
levstepansin-ship-it <levstepansin@gmail.com>
2026-06-12 10:16:55 +0300

    Update admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,13 @@
 ﻿from django.contrib import admin
-from .models import Category, Subcategory, Recipe
+from .models import Category, Subcategory, Recipe, Comment
 
 admin.site.register(Category)
 admin.site.register(Subcategory)
 admin.site.register(Recipe)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'author', 'created_at', 'is_approved')
+    list_filter = ('is_approved', 'recipe')
+    search_fields = ('author', 'text')
+    list_editable = ('is_approved',)
