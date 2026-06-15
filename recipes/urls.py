88838b3482c64ad1b5fa94deaa36b32db0c105ac
levstepansin-ship-it <levstepansin@gmail.com>
2026-06-15 15:39:40 +0300

    Update urls.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
+    path('recipe/<int:recipe_id>/rate/', views.rate_recipe, name='rate_recipe'),
     path('recipe/random/', views.random_recipe, name='random_recipe'),
     path('privacy/', views.privacy, name='privacy'),
     path('search/', views.search, name='search'),
