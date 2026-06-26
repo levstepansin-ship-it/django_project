c3e8d29446f03d05500cb1c33f8ad38021f8ade3
levstepansin-ship-it <levstepansin@gmail.com>
2026-06-26 20:00:28 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ python manage.py collectstatic --noinput
 
 # Применяем миграции к базе данных
 python manage.py migrate
+
+# Создаём суперпользователя (если его нет)
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin123') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell
