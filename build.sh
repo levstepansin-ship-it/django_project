37172df9cc676e75e8980beae0f4dab899e985ad
levstepansin-ship-it <levstepansin@gmail.com>
2026-06-26 18:10:38 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+# Устанавливаем зависимости
+pip install -r requirements.txt
+
+# Собираем статику (картинки, CSS, JS)
+python manage.py collectstatic --noinput
+
+# Применяем миграции к базе данных
+python manage.py migrate
